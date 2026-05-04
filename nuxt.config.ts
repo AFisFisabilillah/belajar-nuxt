09e281525226f26c2b0172a2347aca66bf3c6a12
Afiss <afisfisabilillah21@gmail.com>
2026-05-04 13:30:07 +0700

    fixed bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { createResolver } from '@nuxt/kit'
 import tailwindcss from "@tailwindcss/vite";
-
+const { resolve } = createResolver(import.meta.url)
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css:["app/assets/css/main.css"],
+  css: [resolve('./app/assets/css/main.css')],
   $env:{
 
   },
